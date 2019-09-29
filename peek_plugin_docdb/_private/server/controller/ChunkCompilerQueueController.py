0f77a5a5ada6a2aa1d7e40e5abad5ea46361b5b4
Jarrod Chesney <jarrod.chesney@synerty.com>
2019-09-29 20:58:59 +1000

    Added fix to ensure a chunk isn't compiled twice in parallel
    PEEK-477

--- peek_plugin_docdb/_private/server/controller/ChunkCompilerQueueController.py
+++ peek_plugin_docdb/_private/server/controller/ChunkCompilerQueueController.py
@@ -44,6 +44,8 @@ class ChunkCompilerQueueController:
         self._lastQueueId = -1
         self._queueCount = 0
 
+        self._chunksInProgress = set()
+
     def start(self):
         self._statusController.setCompilerStatus(True, self._queueCount)
         d = self._pollLoopingCall.start(self.PERIOD, now=False)
@@ -79,6 +81,10 @@ class ChunkCompilerQueueController:
         for start in range(0, len(queueItems), self.ITEMS_PER_TASK):
             items = queueItems[start: start + self.ITEMS_PER_TASK]
 
+            # If we're already processing these chunks, then return and try later
+            if self._chunksInProgress & set([o.chunkKey for o in items]):
+                return
+
             # Set the watermark
             self._lastQueueId = items[-1].id
 
@@ -99,6 +105,9 @@ class ChunkCompilerQueueController:
 
         startTime = datetime.now(pytz.utc)
 
+        # Add the chunks we're processing to the set
+        self._chunksInProgress |= set([o.chunkKey for o in items])
+
         try:
             chunkKeys = yield compileDocumentChunk.delay(items)
             logger.debug("Time Taken = %s" % (datetime.now(pytz.utc) - startTime))
@@ -107,9 +116,12 @@ class ChunkCompilerQueueController:
             self._statusController.addToCompilerTotal(len(items))
             self._statusController.setCompilerStatus(True, self._queueCount)
 
+            # Success, Remove the chunks from the in-progress queue
+            self._chunksInProgress -= set([o.chunkKey for o in items])
+
         except Exception as e:
-            self._statusController.setCompilerError(str(e))
-            logger.warning("Retrying compile : %s", str(e))
+            # self._statusController.setCompilerError(str(e))
+            logger.debug("Retrying compile : %s", str(e))
             reactor.callLater(2.0, self._sendToWorker, items)
             return
 
